cf407af98c137a6182acbf58836a1ed829865243
Henrique <rike2807@gmail.com>
2022-05-09 18:10:36 -0300

    first-commit

---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+center:true;
+
+$('.loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
+$('.nonloop').owlCarousel({
+    center: true,
+    items:2,
+    loop:false,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
